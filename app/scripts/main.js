04a94cea4bf7c0d69ecd8c0628a98835784caaa2
Richard Westenra <rjwestenra@gmail.com>
2015-02-12 17:01:06 +0000

    Get a working demo running

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -40,49 +40,29 @@ $(function(){
 
   function gyro(){
 
-    // Position Variables
-    // var x = 0;
-    // var y = 0;
-    // var z = 0;
-
-    // Speed - Velocity
-    // var vx = 0;
-    // var vy = 0;
-    // var vz = 0;
-
-    // Acceleration
-    var ax = 0;
-    var ay = 0;
-    var az = 0;
-    var ai = 0;
-    var arAlpha = 0;
-    var arBeta = 0;
-    var arGamma = 0;
-
     var delay = 100;
-    // var vMultiplier = 0.01;
 
     var alpha = 0;
     var beta = 0;
     var gamma = 0;
 
-    window.ondevicemotion = function(event) {
-      ax = Math.round(Math.abs(event.accelerationIncludingGravity.x * 1));
-      ay = Math.round(Math.abs(event.accelerationIncludingGravity.y * 1));
-      az = Math.round(Math.abs(event.accelerationIncludingGravity.z * 1));    
-      ai = Math.round(event.interval * 100) / 100;
-      var rR = event.rotationRate;
 
-      if (rR !== null) {
-        arAlpha = Math.round(rR.alpha);
-        arBeta = Math.round(rR.beta);
-        arGamma = Math.round(rR.gamma);
-      }
-
-      // ax = Math.abs(event.acceleration.x * 1000);
-      // ay = Math.abs(event.acceleration.y * 1000);
-      // az = Math.abs(event.acceleration.z * 1000);
-    };
+    function left(n, k) {
+      k = k || 50;
+      return (n * k)+ 'px' ;
+    }
+    function z(n) {
+      n = (n / 360) || 0;
+      return left(n);
+    }
+    function x(n) {
+      n = (n / 180) || 0;
+      return left(n);
+    }
+    function y(n) {
+      n = (n / 90) || 0;
+      return left(n);
+    }
 
     window.ondeviceorientation = function(event) {
       alpha = Math.round(event.alpha);
@@ -90,56 +70,44 @@ $(function(){
       gamma = Math.round(event.gamma);
     };
       
-    function d2h(d) {return d.toString(16);}
-    // function h2d(h) {return parseInt(h,16);}
-    
-    function makecolor(a, b, c) {
-      var red = Math.abs(a) % 255;
-      var green = Math.abs(b) % 255;
-      var blue = Math.abs(c) % 255;
-      return '#' + d2h(red) + d2h(green) + d2h(blue);
-    }
-    
-    // function makeacceleratedcolor(a, b, c) {
-    //   var red = Math.round(Math.abs(a + az) % 255);
-    //   var green = Math.round(Math.abs(b + ay) % 255);
-    //   var blue = Math.round(Math.abs(c + az) % 255);
-    //   return '#' + d2h(red) + d2h(green) + d2h(blue);
-    // }
     
     
     if (window.DeviceMotionEvent===undefined) {
-      $('no').show();
-      $('yes').hide();
+
+      $('<p>',{
+        class: 'browsehappy',
+        text: 'Your browser does not support Device Orientation and Motion API. Try this sample with iPhone, iPod or iPad with iOS 4.2+.'
+      }).prependTo('body');
+
     } else {
 
       setInterval(function() {
-        document.getElementById('xlabel').innerHTML = 'X: ' + ax;
-        document.getElementById('ylabel').innerHTML = 'Y: ' + ay;
-        document.getElementById('zlabel').innerHTML = 'Z: ' + az;                   
-        document.getElementById('ilabel').innerHTML = 'I: ' + ai;                   
-        document.getElementById('arAlphaLabel').innerHTML = 'arA: ' + arAlpha;                              
-        document.getElementById('arBetaLabel').innerHTML = 'arB: ' + arBeta;
-        document.getElementById('arGammaLabel').innerHTML = 'arG: ' + arGamma;                                                  
-        document.getElementById('alphalabel').innerHTML = 'Alpha: ' + alpha;
-        document.getElementById('betalabel').innerHTML = 'Beta: ' + beta;
-        document.getElementById('gammalabel').innerHTML = 'Gamma: ' + gamma;
 
-        document.getElementById('accelcolor').innerHTML = 'Color: ' + makecolor(ax, ay, az);
-        document.getElementById('accelcolor').style.background = makecolor(ax, ay, az);
-        document.getElementById('accelcolor').style.color = '#FFFFFF';
-        document.getElementById('accelcolor').style.fontWeight = 'bold';
+        $('#values').text('Alpha: ' + alpha +', Beta: ' + beta + ', Gamma: ' + gamma);
 
-        document.getElementById('gyrocolor').innerHTML = 'Color: ' + makecolor(alpha, beta, gamma);
-        document.getElementById('gyrocolor').style.background = makecolor(alpha, beta, gamma);
-        document.getElementById('gyrocolor').style.color = '#FFFFFF';
-        document.getElementById('gyrocolor').style.fontWeight = 'bold';
+        $('#alpha')
+          .text('Alpha: ' + alpha)
+          .css({
+            'left': z(alpha,100)
+          });
 
-        document.bgColor = makecolor(arAlpha, arBeta, arGamma);
+        $('#beta')
+          .text('Beta: ' + beta)
+          .css({
+            'left': x(beta)
+          });
+
+        $('#gamma')
+          .text('Gamma: ' + gamma)
+          .css({
+            'left': y(gamma)
+          });
 
       }, delay);
+
     }
   }
+
   gyro();
 
 
