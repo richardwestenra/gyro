ec836b3b706022a8f72403b407b76c580ee90749
Richard Westenra <rjwestenra@gmail.com>
2015-02-13 16:51:58 +0000

    Add glove texture and ambient light

--- app/scripts/threeDemo.js
+++ app/scripts/threeDemo.js
@@ -35,34 +35,40 @@ $(function(){
 	$container.append(renderer.domElement);
 
 	// create the sphere's material
-	var sphereMaterial = new THREE.MeshLambertMaterial(
-	{
-		color: 0xCC0000
-	});
+	// var sphereMaterial = new THREE.MeshLambertMaterial(
+	// {
+	// 	color: 0xCC0000
+	// });
+
 
 	// set up the sphere vars
 	var radius = 50, segments = 16, rings = 16;
 
 	// create a new mesh with sphere geometry -
 	// we will cover the sphereMaterial next!
-	var sphere = new THREE.Mesh(
-		new THREE.SphereGeometry(
-			radius,
-			segments,
-			rings
-		),
-		sphereMaterial
-	);
+	// var sphere = new THREE.Mesh(
+	// 	new THREE.SphereGeometry(
+	// 		radius,
+	// 		segments,
+	// 		rings
+	// 	),
+	// 	sphereMaterial
+	// );
+
+	var geometry   = new THREE.SphereGeometry(radius, segments, rings);
+	var material  = new THREE.MeshPhongMaterial();
+	material.map = THREE.ImageUtils.loadTexture('images/earthmap1k.jpg');
+	var earthMesh = new THREE.Mesh(geometry, material);
 
 	// add the sphere to the scene
-	scene.add(sphere);
+	scene.add(earthMesh);
+	// scene.add(sphere);
 
 	// and the camera
 	scene.add(camera);
 
 	// create a point light
 	var pointLight = new THREE.PointLight( 0xFFFFFF );
-
 	// set its position
 	pointLight.position.x = 10;
 	pointLight.position.y = 50;
@@ -71,12 +77,18 @@ $(function(){
 	// add to the scene
 	scene.add(pointLight);
 
+	var light = new THREE.AmbientLight( 0x777777 );
+	scene.add(light);
+
 	// draw!
 	renderer.render(scene, camera);
 
 	$('canvas').on('mousemove',function(e) {
 		pointLight.position.x = e.offsetX - WIDTH/2;
 		pointLight.position.y = HEIGHT/2 - e.offsetY;
+		earthMesh.rotation.y = (e.offsetX - WIDTH/2) / 100;
+		earthMesh.rotation.x = (e.offsetY - WIDTH/2) / 100;
+		// camera.rotation.y = ((e.offsetX - WIDTH/2) * (Math.PI / 180)) / 50;
 		renderer.render(scene, camera);
 	});
 	
